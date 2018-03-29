6f901a8b6d738d21c9c89c5dc7fa8e3e0aa500fc
Heavenel Cerna <heavenelcerna@gmail.com>
2018-03-29 14:07:49 -0600

    Update

---
+++ game_files/source/SNES_driver.s
@@ -0,0 +1,14 @@
+@ Buttons
+B1:	    .asciz		"B\n"
+B2:	    .asciz		"Y\n"
+B3:	    .asciz		"Select\n"
+B4:	    .asciz 		"Start\n"
+B5:	    .asciz		"Joy-pad UP\n"
+B6:	    .asciz		"Joy-pad DOWN\n"
+B7:	    .asciz		"Joy-pad LEFT\n"
+B8:	    .asciz		"Joy-pad RIGHT\n"
+B9:	    .asciz		"A\n"
+B10:	.asciz		"X\n"
+B11:	.asciz		"Left\n"
+B12:	.asciz		"Right\n"
+
