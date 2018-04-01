0ed606e2d3de0b3ded926b5ae2db94a78482699c
austin-so <36573917+austin-so@users.noreply.github.com>
2018-03-31 19:40:09 -0600

    Update main_menu.s

--- game_files/source/main_menu.s
+++ game_files/source/main_menu.s
@@ -17,10 +17,18 @@ menu_quit	.req	r12
 .int 768
 
 @ Implementation:			Implemented?
-
+@A tile might be rectangle/square with variable colour values.
+@Use Image-to-ASCII Java application on D2L:
+@Convert a tile to a colour bitmap.
+@Generated ascii values are colour values in row-major order.
+@Load colour values and store in the framebuffer.
+@You need to know width/height of your tile.
 mm:
 	ldr r5, =framebufferinfo
 	bl initFbinfo
+	
+	ldr r5, =menu_start		@loadframe buffer with meu_start image
+	
 @ Draw bkgd, title, authors		N
 
 @ Switch between 2 images		N
@@ -33,3 +41,6 @@ mm:
 
 .global	draw_menu
 draw_menu:	
+
+
+
