bf7ceafe89d5f2b8f175451841004341f716d114
kevinhuynh <kevinhuynh8@outlook.com>
2018-04-02 15:47:31 -0600

    Update main_menu.s

--- game_files/source/main_menu.s
+++ game_files/source/main_menu.s
@@ -8,13 +8,7 @@ SNES_controller	.req	r10
 menu_start	.req	r11
 menu_quit	.req	r12
 
-.global
-.align 4
-
-.int 8
-.int 8
-.int 720
-.int 960
+.global	start_menu
 
 @ Implementation:			Implemented?
 @A tile might be rectangle/square with variable colour values.
@@ -34,48 +28,20 @@ menu_quit	.req	r12
 	@ Quit Game => clear + exit	N
 	
 start_menu:
+	push	{r4-r10, lr}
+
+	ldr	r0, =100000		@ Delay 0.1s
+	bl	delayMicroseconds
+
 	mov r0, #100
 	mov r1, #100
-	
 	ldr r2, =menu_start		@loadframe buffer with menu_start image
 	bl draw_menu
 	
-halt:
-	b halt
-
-
-.global	draw_menu
-draw_menu:	
-	push	{r4, r5)
+	pop	{r4-r10, fp, pc}
 	
-	offset	.req	r4
-	ldr	r5, =frameBufferInfo
-	
-	ldr	r3, [r5,#4]
-	mul	r1, r3
-	add	offset, r0, r1
-	
-	lsl 	offset, #2
-	
-	ldr	r0,[r5]
-	str	r2,[r0, offset]
-	
-	pop	{r4, r5}
-	bx	lr	
 
 .section .data
-
 .align
-.global frameBufferInfo
 
-framebufferInfo:
- .int	0
- .int	720
- .int	960
-	
-	
-	
-	
-	
-	
 	
