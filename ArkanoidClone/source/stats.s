2c80aff42bef0fd3c4b0be00812908fcb1d5c7e8
kevinhuynh <kevinhuynh8@outlook.com>
2018-04-02 21:21:31 -0600

    Update and rename score.s to stats.s
    Code that needs to be changed (i.e. resolution/placement or something):
    initScore
    initLives
    updateStats
    
    Changed documentation
    updateScoreAndLives -> updateStats
            makeTile -> drawCell
    intToString -> toString
            Changed logic
            push/pop {r4,r5,lr/pc} -> mov pc, lr
            addge r3, r3, #1 -> r3, #1
            changed the logic
    Moved WIN, LOSE, and resetScore to game.s

---
+++ ArkanoidClone/source/stats.s
@@ -0,0 +1,132 @@
+@ Game stats
+
+.section	.text
+
+
+.global initScore, initLives, updateStats, toString
+
+@ Initiates score
+@ r0 - character
+@ r1 - x
+@ r2 - y
+@ r3 - color
+
+initScore:
+	ldr	r0, =scoreChar
+	mov	r1, #88
+	mov	r2, #864
+	ldr	r3, =cWhite
+	bl	drawWord
+	
+	mov	pc, lr
+
+
+@ Initiates lives
+@ r0 - character
+@ r1 - x
+@ r2 - y
+@ r3 - color
+
+initLives:
+	ldr	r0, =livesChar
+	mov	r1, #468
+	mov	r2, #864
+	ldr	r3, =cWhite
+	bl	drawWord
+	
+	mov	pc, lr
+
+
+@ Update score and lives
+
+updateStats:
+	push	{r4, lr}
+
+	@ Erases the current score
+	mov	r0, #160
+	mov	r1, #863
+	mov	r2, #0x0
+	mov	r3, #32
+	mov	r4, r3
+	bl	drawCell
+
+	@ Erases the current lives
+	mov	r0, #544
+	mov	r1, #863
+	mov	r2, #0x0
+	mov	r3, #32
+	mov	r4, r3
+	bl	drawCell
+
+	@ Draws updated score and lives
+	@ r0 - First digit
+	@ r1 - Second digit
+
+	@ Score
+	ldr	r0, =score
+	bl	tostring
+	mov	r4, r1
+
+	mov	r1, #165
+	mov	r2, #864
+	bl	drawChar
+
+	mov	r0, r4
+	mov	r1, #176
+	mov	r2, #864
+	bl	drawChar
+
+	@ Lives
+	ldr	r0, =lives
+	bl	tostring		
+	mov	r4, r1			
+
+	mov	r1, #545
+	mov	r2, #864
+	bl	drawChar
+
+	mov	r0, r4
+	mov	r1, #556
+	mov	r2, #864
+	bl	drawChar
+		
+	pop	{r4, pc}
+
+
+@ Changes the score and lives number to string from integer
+@ r0 - location of the integer
+@ returns r0 (string code)
+
+toString:	
+	ldr	r0, [r0]
+	mov	r2, #0
+	
+@ Calculates the number's first and second digit
+calcDigits:
+	cmp	r0, #10
+	blt	convertASCII
+	addge	r2, #1
+	subge	r0, #10
+	
+	b	calcDigits
+
+@ Converts to ASCII
+convertASCII:
+	add	r1, r0, #48		@ r1 - Second digit
+	add	r0, r2, #48		@ r0 - First digit
+
+	mov	pc, lr
+
+
+
+@@@@@-----Data-----@@@@@
+
+.section	.data
+
+scoreChar:	.asciz		"SCORE: "
+livesChar:	.asciz		"LIVES: "
+
+.global	score, lives
+	
+score:	.int	0
+lives:	.int	3
