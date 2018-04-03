ee8bff97835885ce748dfedcd7ea96438853d15f
Heavenel Cerna <heavenelcerna@gmail.com>
2018-04-03 03:43:24 -0600

    Working Code
    Needs renaming and restructuring

---
+++ ArkanoidClone/drawing.s
@@ -0,0 +1,216 @@
+@@@@@@@@@@@@@@@@@@@@@@@@@ Code Section @@@@@@@@@@@@@@@@@@@@@@@@@
+.text
+.global Init_Frame
+Init_Frame:
+		push	{lr}
+
+		ldr	r0, =frameBufferInfo
+		bl	initFbInfo
+
+		pop	{pc}
+
+@ r0 - xStart
+@ r1 - yStart
+@ r2 - color
+@ r3 - length
+@ r4 - xLength (height)
+.global drawCell
+drawCell:
+	length	.req 	r3
+	pxDrawn	.req	r6
+	offset	.req	r4
+	frame	.req	r5
+	width	.req	r6
+	xval	.req	r7
+	yval	.req	r8
+	color	.req	r9
+
+	push	{r5-r6, lr}
+	@ length r3
+	height	.req	r4
+
+	ldr	r5, =frameBufferInfo
+	ldr	r5, [r5, #4]
+
+	mov	pxDrawn, #0
+	tileLoop:
+		bl	drawHLn
+		add	r0, r0, r5
+		sub	r0, r0, length
+		sub	r0, r0, #1
+		add	pxDrawn, pxDrawn, #1
+		cmp	pxDrawn, height
+		ble	tileLoop
+
+	pop	{r5-r6, pc}
+
+.global drawPx
+@ r0 - xStart
+@ r1 - yStart
+@ r2 - color
+
+drawPx:
+	push	{r3-r9, lr}
+
+	mov	xval, r0
+	mov	yval, r1
+	mov	color, r2
+
+	ldr	frame, =frameBufferInfo
+	ldr	width, [frame, #4]
+
+	@ making the offset
+	mul	yval, width
+	add	offset, xval, yval
+	lsl	offset, #2	@ * 4
+
+	ldr	r3, [frame]
+	str	color, [r3, offset]
+
+	pop	{r3-r9, pc}
+
+
+	.unreq	offset
+	.unreq	frame
+	.unreq	width
+
+@ r0 - xStart
+@ r1 - yStart
+@ r2 - color
+@ r3 - length
+.global drawHLn
+drawHLn:
+	push	{r6, lr}
+
+	mov	pxDrawn, #0
+	hlPxLoop:
+		bl	drawPx
+		add	r0, r0, #1
+		add	pxDrawn, pxDrawn, #1
+		cmp	pxDrawn, length
+		ble	hlPxLoop
+
+	pop	{r6, lr}
+	mov	pc, lr
+
+
+	.unreq	length
+	.unreq	height
+	.unreq	pxDrawn
+
+@ Draw the character in r0
+@ r1=x
+@ r2=y
+@ r3=colour
+.global drawChar
+drawChar:
+	push		{r4-r9, lr}
+
+	chAdr		.req	r4
+	px		.req	r5
+	py		.req	r6
+	row		.req	r7
+	mask		.req	r8
+        colour		.req	r9
+
+	ldr		chAdr, =font		@ load the address of the font map
+	add		chAdr,	r0, lsl #4	@ char address = font base + (char * 16)
+
+	mov		py, r2			@ init the Y coordinate (pixel coordinate)
+
+	ldr		r2 , =initX
+	str		r1, [r2]
+
+	charLoop:
+		ldr		px, =initX
+		ldr		px, [px]		@ init the X coordinate
+		mov		mask, #0x01		@ set the bitmask to 1 in the LSB
+		LDRB		row, [chAdr], #1	@ load the row byte, post increment chAdr
+
+	rowLoop:
+		tst		row, mask		@ test row byte against the bitmask
+		Beq		noPixel
+
+		mov		r0, px
+		mov		r1, py
+		mov		r2, colour
+		bl		drawPx			@ draw pixel at (px, py)
+
+	noPixel:
+		add		px, px, #1		@ increment x coordinate by 1
+		lsl		mask, #1		@ shift bitmask left by 1
+
+		tst		mask,	#0x100		@ test if the bitmask has shifted 8 times (test 9th bit)
+		Beq		rowLoop
+
+		add		py,py, #1		@ increment y coordinate by 1
+
+		tst		chAdr, #0xF
+		Bne		charLoop		@ loop back to charLoop$, unless address evenly divisibly by 16 (ie: at the next char)
+
+	.unreq	chAdr
+	.unreq	px
+	.unreq	py
+	.unreq	row
+	.unreq	mask
+
+	pop		{r4-r9, pc}
+
+
+@ r0 - char array address
+@ r1 - initial x
+@ r2 - y
+@ r3 - color
+.global drawWord
+drawWord:
+	push	{r4-r7, lr}
+	mov	r5, r0
+	mov	r6, r1
+	mov	r7, r2
+	mov	r4, r3
+	drawWordLoop:
+		LDRB	r0, [r5], #1
+		cmp	r0, #0
+		popeq	{r4-r7, pc}
+
+		mov	r1, r6
+		mov	r2, r7
+		mov	r3, r4
+		bl	drawChar
+		add	r6, r6, #11
+		B	drawWordLoop
+
+.global blackScreen @blacks out game screen takes and returns no arguments
+blackScreen:
+	push {r4, r5, lr}
+	mov r4, #0
+	mov r5, #0
+
+drawblackscreen:
+	mov	r0, r4
+	mov	r1, r5
+	mov	r2, #0
+	bl	drawPx
+
+	add	r4, r4, #1
+    	cmp 	r4, #720
+    		moveq	r4, #0
+   	 	addeq r5, r5, #1
+
+    cmp   r5, #960
+    blt drawblackscreen
+    pop {r4,r5, pc}
+
+@@@@@@@@@@@@@@@@@@@@@@@@@ Data Section @@@@@@@@@@@@@@@@@@@@@@@@@
+.section .data
+
+.align 4
+font:		.incbin	"font.bin"
+
+initX:		.int 0
+
+.global frameBufferInfo
+frameBufferInfo:
+	.int	0	@ frame buffer pointer
+	.int	0	@ width
+	.int	0	@ height
---
+++ ArkanoidClone/valuepack.s
@@ -0,0 +1,276 @@
+@@@@@@@@@@@@@@@@@@@@@@@@@ Text Section @@@@@@@@@@@@@@@@@@@@@@@@@
+.section	.text
+
+@ listens for drops
+.global dropListener
+dropListener:
+	push	{r4-r6, lr}
+		bl	tryCatchBall
+		bl	tryPaddle
+	pop	{r4-r6, pc}
+
+
+@ checks whether paddle drop or catch ball drop is occuring
+
+	tryPaddle:
+		push	{lr}
+
+		ldr	r0, =paddleDropState
+		ldr	r0, [r0]
+
+		cmp	r0, #1
+			bleq	bigPaddleDrop
+			blLT	paddleTileBroken
+		pop	{pc}
+
+	tryCatchBall:
+		push	{lr}
+
+		ldr	r0, =ballDropState
+		ldr	r0, [r0]
+
+		cmp	r0, #1
+			bleq	catchBallDrop
+			blLT	ballTileBroken
+		pop	{pc}
+
+@ checks whetehr the brick holding the approrpaite til has been broken
+
+ballTileBroken:
+	push	{r4-r6,lr}
+
+	mov	r5, #1
+
+	ldr	r0, =tile26
+	LDRB	r6, [r0]
+
+	cmp	r6, #0
+		ldreq	r0, =ballDropState
+		streq	r5, [r0]
+
+	pop	{r4-r6,pc}
+
+
+
+paddleTileBroken:
+	push	{r4-r6,lr}
+
+	mov	r5, #1
+
+	ldr	r0, =tile20
+	LDRB	r6, [r0]
+
+	cmp	r6, #0
+		ldreq	r0, =paddleDropState
+		streq	r5, [r0]
+
+	pop	{r4-r6,pc}
+
+@ drops the value pack inrementally
+bigPaddleDrop:
+	push	{r4-r8, lr}
+
+	mov	r0, #56
+
+	ldr	r1, =paddleDropY
+	ldr	r6, [r1]
+
+	@ draws white tile
+	mov	r1, r6
+	mov	r2, #0xFFFFFF
+	mov	r3, #28
+	mov	r4, r3
+	bl	drawCell
+
+	add	r7, r6, #32
+	ldr	r1, =paddleDropY
+	str	r7, [r1]
+
+	@ draws signifying value
+	mov	r0, #'+'
+	mov	r1, #64
+	add	r2, r6, #4
+	bl	drawChar
+
+	mov	r0, #56
+	sub	r1, r6, #32
+	mov	r2, #0x0
+	mov	r3, #28
+	mov	r4, r3
+	bl	drawCell
+
+	ldr	r0, =paddleDropY
+	ldr	r0, [r0]
+	mov	r1, #774
+
+
+	@ if drop is near the bottom check if the paddle caught it
+	cmp	r0, r1
+	blGE	checkPaddleDrop
+
+
+	pop	{r4-r8, pc}
+
+@ check whether the paddle drop is caught
+checkPaddleDrop:
+	push	{lr}
+
+	ldr	r0, =paddleDropState
+	mov	r1, #2
+	str	r1, [r0]
+
+	@ load paddle position
+	ldr	r0, =paddlePosition
+	ldr	r0, [r0]
+
+	@ if paddle is 88 from the left
+	cmp	r0, #88
+	blLE	bigPaddle	@ change paddle to big paddle
+
+	mov	r0, #56
+	ldr	r1, =paddleDropY
+	ldr	r1, [r1]
+	sub	r1, r1, #32
+	mov	r2, #0x0
+	mov	r3, #28
+	mov	r4, r3
+	bl	drawCell
+
+	pop	{pc}
+
+catchBallDrop:
+	push	{r4-r8, lr}
+
+	mov	r0, #428
+
+	ldr	r1, =ballDropY
+	ldr	r6, [r1]
+
+	@ create the white tile
+	mov	r1, r6
+	mov	r2, #0xFFFFFF
+	mov	r3, #28
+	mov	r4, r3
+	bl	drawCell
+
+	add	r7, r6, #32
+	ldr	r1, =ballDropY
+	str	r7, [r1]
+
+	@ create the signifyuing character
+	mov	r0, #'-'
+	mov	r1, #434
+	add	r2, r6, #4
+	bl	drawChar
+
+	mov	r0, #428
+	sub	r1, r6, #32
+	mov	r2, #0x0
+	mov	r3, #28
+	mov	r4, r3
+	bl	drawCell
+
+	ldr	r0, =ballDropY
+	ldr	r0, [r0]
+	mov	r1, #774
+
+
+	@ if drop is near the bottom check if the paddle caught it
+	cmp	r0, r1
+	blGE	checkBallDrop
+
+	pop	{r4-r8, pc}
+
+@ cgecj whether ball drop is caught
+checkBallDrop:
+	push	{lr}
+
+	ldr	r0, =ballDropState
+	mov	r1, #2
+	str	r1, [r0]
+
+	@ load paddle position
+	ldr	r0, =paddlePosition
+	ldr	r0, [r0]
+
+	@ if paddle is 428 from the left
+	cmp	r0, #428
+	blLE	enableCatchBall	@ change to catch ball
+	BGT	tryOtherSide
+
+	checkBallDrop2:
+		mov	r0, #428
+		ldr	r1, =ballDropY
+		ldr	r1, [r1]
+		sub	r1, r1, #32
+		mov	r2, #0x0
+		mov	r3, #28
+		mov	r4, r3
+		bl	drawCell
+		pop	{pc}
+
+	tryOtherSide:
+		ldr	r1, =paddleSize
+		ldr	r1, [r1]
+
+		add	r0, r0, r1
+		cmp	r0, #428
+		blGE	enableCatchBall
+
+		B	checkBallDrop2
+
+
+@ debuging purposes only
+.global testPaddle
+testPaddle:
+	push	{lr}
+
+	ldr	r0, =paddleDropState
+	mov	r1, #1
+	str	r1, [r0]
+
+	pop	{pc}
+
+.global	testBall
+testBall:
+	push	{lr}
+
+	ldr	r0, =ballDropState
+	mov	r1, #1
+	str	r1,[r0]
+
+	pop	{pc}
+
+@ resets the state values for value packs for restarting
+.global	resetValuePacks
+resetValuePacks:
+	ldr	r0, =paddleDropY
+	mov	r1, #192
+	str	r1, [r0]
+
+	ldr	r0, =ballDropY
+	mov	r1, #192
+	str	r1, [r0]
+
+	ldr	r0, =paddleDropState
+	mov	r1, #0
+	str	r1, [r0]
+
+	ldr	r0, =ballDropState
+	mov	r1, #192
+	str	r1, [r0]
+
+	mov	pc, lr
+
+@@@@@@@@@@@@@@@@@@@@@@@@@ Data Section @@@@@@@@@@@@@@@@@@@@@@@@@
+.section	.data
+
+	paddleDropY:		.int    192
+	ballDropY:		.int	192
+
+
+	@ 0 - default
+	@ 1 - dropping
+	@ 2 - caught/finished
+	paddleDropState:	.int	0
+	ballDropState:		.int	0
